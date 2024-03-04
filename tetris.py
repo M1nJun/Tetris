be9716d03ba4364d323f73122e6d1c91d16654eb
Minjun Lee <lmjg0407@gmail.com>
2024-03-04 16:06:11 -0600

    figured out scores and drop speed changes, and game end conditions. Want to put music and an opening screen if possible

--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import pygame
 from random import choice
+from os import path
 
 FPS = 200
 clock = pygame.time.Clock()
@@ -22,9 +23,6 @@ WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
 WINDOW_HEIGHT = GAME_HEIGHT + PADDING * 2
 
 # game behaviour 
-UPDATE_START_SPEED = 200
-MOVE_WAIT_TIME = 200
-ROTATE_WAIT_TIME = 200
 BLOCK_OFFSET = pygame.Vector2(COLS // 2, -1)
 
 # Colors 
@@ -57,16 +55,21 @@ TETROMINOS = {
 	'Z': {'shape': [(0,0), (1,0), (0,-1), (-1,-1)], 'color': RED}
 }
 
-SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
+SCORE_DATA = {1: 50, 2: 100, 3: 500, 4: 1500}
+
+DROP_SPEED = {1: 60, 2: 30, 3: 20, 4: 10}
 
 class Game:
-    def __init__(self):
+    def __init__(self, get_next_shape, update_score):
 
         # general
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
         self.rect = self.surface.get_rect(topleft = (PADDING,PADDING))
         self.sprites = pygame.sprite.Group()
+
+        self.get_next_shape = get_next_shape
+        self.update_score = update_score
 
         # was going to do tuple for the pos argument, but pygame had it's own vector implementation
         # self.block = Block(self.sprites, pygame.Vector2(3,5), 'blue')
@@ -81,14 +84,35 @@ class Game:
             self.sprites,
             self.spawn_new_tetromino,
             self.occupancy)
+        
+        self.current_level = 1
+        self.current_score = 0
+        self.current_lines = 0
+
+    def calculate_score(self, num_lines):
+        self.current_lines += num_lines
+        # the higher the level, the higher the score you will get from the num lines you have cleared
+        self.current_score += SCORE_DATA[num_lines] * self.current_level
+
+        # every 10 lines += level by 1
+        if self.current_lines / 10 > self.current_level:
+            self.current_level += 1
+        self.update_score(self.current_lines, self.current_score, self.current_level)
+
+    def check_game_over(self):
+        for block in self.tetromino.blocks:
+            if block.pos.y < 0:
+                exit()
 
     # had to create a new argument for tetromino class to shift to allow new tetromino to spawn when curr tetromino hits the bottom
     def spawn_new_tetromino(self):
-
+        # this is checking the previous tetromino. not the new one we're trying to create
+        self.check_game_over()
         self.check_full_rows()
         self.tetromino = Tetromino(
             # 'I', # for testing purposes
-            choice(list(TETROMINOS.keys())),
+            # this first arg used to be: choice(list(TETROMINOS.keys())),
+            self.get_next_shape(),
             self.sprites,
             self.spawn_new_tetromino,
             self.occupancy)
@@ -175,6 +199,8 @@ class Game:
             for block in self.sprites:
                 self.occupancy[int(block.pos.y)][int(block.pos.x)] = block
 
+            # for updating score
+            self.calculate_score(len(delete_rows))
 
 class Tetromino:
     def __init__(self, shape, group, spawn_new_tetromino, occupancy):
@@ -304,12 +330,40 @@ class Block(pygame.sprite.Sprite):
 
 class Preview:
     def __init__(self):
+        # get window
         self.display_surface = pygame.display.get_surface()
         self.surface = pygame.Surface((SIDEBAR_WIDTH, GAME_HEIGHT * PREVIEW_HEIGHT_R))
         self.rect = self.surface.get_rect(topright = (WINDOW_WIDTH - PADDING, PADDING))
 
-    def run(self):
+        # these are just letters
+        # need dictionary of form {'L' : imported png of the tetromino}
+        # a hardcoded path to the images isn't a great idea because different os can work differently
+        # path join does the job of melting the path down to the right form
+        # convert_alpha makes the image work better with pygame
+        self.shape_surfaces = {shape: pygame.image.load(path.join('.', 'tetrominos', f'{shape}.png')).convert_alpha() for shape in TETROMINOS.keys()}
+        # print(self.shape_surfaces)
+
+        # want 1/3 of the preview height to place three items equaly on the screen
+        self.partition_height = self.surface.get_height() / 3
+
+    def run(self, next_shapes):
+
+        self.surface.fill(GRAY)
+        self.display_next_shapes(next_shapes)
+
+        # displays the the surface on top of the display surface
         self.display_surface.blit(self.surface, self.rect)
+        pygame.draw.rect(self.display_surface, WHITE, self.rect, 2, 2)
+
+    def display_next_shapes(self, shapes):
+        for i, shape in enumerate(shapes):
+            #this surface is a local variable, different from the self.surface, which is an attribute of the preview class
+            shape_surface = self.shape_surfaces[shape]
+            x = self.surface.get_width() / 2
+            y = self.partition_height / 2 + i * self.partition_height
+            # met with a problem that the shapes appear starting from the x,y coords, so came up with a rect
+            rect = shape_surface.get_rect(center = (x,y))
+            self.surface.blit(shape_surface, rect)
 
 class Score:
     def __init__(self):
@@ -317,8 +371,30 @@ class Score:
         self.rect = self.surface.get_rect(bottomright = (WINDOW_WIDTH - PADDING, WINDOW_HEIGHT - PADDING))
         self.display_surface = pygame.display.get_surface()
 
+        # font
+        self.font = pygame.font.Font(path.join('.','PressStart.ttf'), 15)
+
+        self.partition_height = self.surface.get_height() / 3
+
+        self.score = 0
+        self.level = 1
+        self.lines = 0
+
+    def display_text(self, pos, text):
+        text_surface = self.font.render(f'{text[0]}: {text[1]}', True, 'white')
+        text_rect = text_surface.get_rect(center = pos)
+        self.surface.blit(text_surface, text_rect)
+
     def run(self):
+
+        self.surface.fill(GRAY)
+        for i, text in enumerate([('Score', self.score), ('Level', self.level), ('Lines', self.lines)]):
+            x = self.surface.get_width() / 2
+            y = self.partition_height / 2 + i * self.partition_height
+            self.display_text((x,y),text)
+
         self.display_surface.blit(self.surface, self.rect)
+        pygame.draw.rect(self.display_surface, WHITE, self.rect, 2, 2)
 
 class Main:
     def __init__(self):
@@ -328,16 +404,35 @@ class Main:
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('TETRIS')
 
-        self.game = Game()
+        # create mutiple shapes for preview
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+
+        self.game = Game(self.get_next_shape, self.update_score)
         self.score = Score()
         self.preview = Preview()
+
+        # music
+        # self.music = pygame.mixer.Sound(path.join('.', 'audio', ''))
+
+    def update_score(self, lines, score, level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+
+    def get_next_shape(self):
+        # pop removes the first item from the list and returns it
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
     
     def run(self):
         block_fall_clock = 0
         block_move_clock = 0
         block_rotate_clock = 0
         while True:
-            block_fall_clock = (block_fall_clock+1)%60
+            
+            # block_fall_clock = (block_fall_clock+1)%60
+            block_fall_clock = (block_fall_clock+1)%DROP_SPEED[self.score.level]
             block_move_clock = (block_move_clock+1)%4
             block_rotate_clock = (block_rotate_clock+1)%7
 
@@ -350,7 +445,7 @@ class Main:
 
             self.game.run(block_fall_clock, block_move_clock, block_rotate_clock)
             self.score.run()
-            self.preview.run()
+            self.preview.run(self.next_shapes)
 
             pygame.display.update()
 
